66b9c47bb7518b5d6ce4193562baea0713ed6c58
Tyler CH <tciapala-hazlerig@zagmail.gonzaga.edu>
2025-02-24 22:28:08 -0800

    add csharp.nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,24 @@
+local ensure_installed = require 'mason-lspconfig.ensure_installed'
 -- You can add your own plugins here or in other files in this directory!
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    -- 'seblyng/roslyn.nvim',
+    -- ft = 'cs',
+    -- opts = {
+    --   -- your configuration comes here; leave empty for default settings
+    -- },
+    'iabdelkareem/csharp.nvim',
+    dependencies = {
+      { 'williamboman/mason.nvim', opts = { ensure_installed = { 'csharpier', 'netcoredbg' } } },
+      'mfussenegger/nvim-dap',
+      'Tastyep/structlog.nvim', -- Optional, but highly recommended for debugging
+    },
+    config = function()
+      require('mason').setup() -- Mason setup must run before csharp, only if you want to use omnisharp
+      require('csharp').setup()
+    end,
+  },
+}
