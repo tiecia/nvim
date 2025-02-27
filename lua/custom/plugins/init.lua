7498d735707032be6bbd4398e30ec8e705f6663a
tiecia <tciapala-hazlerig@zagmail.gonzaga.edu>
2025-02-27 13:03:10 -0800

    mason.nvim works

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -5,21 +5,25 @@ local ensure_installed = require 'mason-lspconfig.ensure_installed'
 -- See the kickstart.nvim README for more information
 return {
   {
-    -- 'seblyng/roslyn.nvim',
-    -- ft = 'cs',
-    -- opts = {
-    --   -- your configuration comes here; leave empty for default settings
-    -- },
-    'iabdelkareem/csharp.nvim',
+    'seblyng/roslyn.nvim',
+    ft = 'cs',
     dependencies = {
-      'williamboman/mason.nvim',
-      -- { 'williamboman/mason.nvim', opts = { ensure_installed = { 'csharpier', 'netcoredbg', 'omnisharp' } } },
-      'mfussenegger/nvim-dap',
-      'Tastyep/structlog.nvim', -- Optional, but highly recommended for debugging
+      -- Mason with the registey github:Crashdummyy/mason-registry should be listed as a dependency, but that seems to override other mason configurations.
     },
-    config = function()
-      require('mason').setup() -- Mason setup must run before csharp, only if you want to use omnisharp
-      require('csharp').setup()
-    end,
+    opts = {
+      -- your configuration comes here; leave empty for default settings
+    },
+
+    -- 'iabdelkareem/csharp.nvim',
+    -- dependencies = {
+    --   'williamboman/mason.nvim',
+    --   -- { 'williamboman/mason.nvim', opts = { ensure_installed = { 'csharpier', 'netcoredbg', 'omnisharp' } } },
+    --   'mfussenegger/nvim-dap',
+    --   'Tastyep/structlog.nvim', -- Optional, but highly recommended for debugging
+    -- },
+    -- config = function()
+    --   require('mason').setup() -- Mason setup must run before csharp, only if you want to use omnisharp
+    --   require('csharp').setup()
+    -- end,
   },
 }
