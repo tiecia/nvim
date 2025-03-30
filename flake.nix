57b6ad9af6f5d276c5b59fe8ce623485f607cfac
tiecia <ty.cia@outlook.com>
2025-03-29 23:01:27 -0700

    add flake

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "A basic flake with a shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.systems.url = "github:nix-systems/default";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.systems.follows = "systems";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+      in
+      {
+        packages = rec {
+          neovim = pkgs.stdenv.mkDerivation {
+            name = "tiecia-neovim";
+            src = ./.;
+            buildPhase = ''
+              cp -r $src $out
+            '';
+          };
+
+          default = neovim;
+        };
+
+        devShells.default = pkgs.mkShell { packages = [ pkgs.bashInteractive ]; };
+      }
+    );
+}
