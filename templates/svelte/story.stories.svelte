a27802a18f86661f098ae030cbcb35c84e507ad9
Tyler C <tyler@rosepoint.com>
2026-01-19 10:27:32 -0800

    svelte templates

---
+++ templates/svelte/story.stories.svelte
@@ -0,0 +1,25 @@
+<script lang="ts" module>
+	import { defineMeta } from "@storybook/addon-svelte-csf";
+	import Component from "./{{_variable_}}.svelte";
+
+	const { Story } = defineMeta({
+		title: "Components/{{_cursor_}}",
+		component: Component,
+		tags: ["autodocs"],
+		parameters: {
+			docs: {
+				description: {
+					component: ``,
+				},
+			},
+		},
+	});
+</script>
+
+<Story name="Without test" args={{}} />
+
+<Story
+	name="With test"
+	args={{}}
+	play={async ({ canvasElement, args }) => {}}
+/>
